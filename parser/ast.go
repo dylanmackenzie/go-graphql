1ea34700089074ca41f87c64673db143d0e97f99
Dylan MacKenzie <dylanxmackenzie@gmail.com>
2015-09-25 19:23:07 -0700

    Add documentation for AST

--- parser/ast.go
+++ parser/ast.go
@@ -1,27 +1,31 @@
+// Package parser contains a lexer, parser, and AST for GraphQL
+// documents.
 package parse
 
-// Document is a GraphQL document consisting of a series of definitions
+// Document is a GraphQL document consisting of a series of definitions.
+// It is the root node in the AST.
 type Document struct {
 	Definitions Definitions
 }
 
+// A slice of Definition.
 type Definitions []Definition
 
-// Definition is
+// A single definition in a GraphQL document.
 type Definition interface {
 	Node
 	definition()
 }
 
+// The type of a given operation (either "query" or "mutation").
 type OperationType uint8
 
 const (
 	QUERY OperationType = iota
 	MUTATION
-
-	FRAGMENT
 )
 
+// A Definition representing some type of operation on the dataset.
 type OperationDefinition struct {
 	Name         string
 	OpType       OperationType
@@ -30,6 +34,8 @@ type OperationDefinition struct {
 	SelectionSet SelectionSet
 }
 
+// A Definition representing the structure of some data which can be
+// composed to make other Definitions.
 type FragmentDefinition struct {
 	Name         string
 	Type         string
@@ -40,7 +46,10 @@ type FragmentDefinition struct {
 func (*FragmentDefinition) definition()  {}
 func (*OperationDefinition) definition() {}
 
+// A slice of Variable.
 type Variables []Variable
+
+// A Variable is the declaration of a GraphQL variable.
 type Variable struct {
 	Name     string
 	Type     string
@@ -48,9 +57,10 @@ type Variable struct {
 	Default  Value
 }
 
-// Selection
-
+// A SelectionSet is a slice of Selection.
 type SelectionSet []Selection
+
+// A Selection is a group of fields which will be used in an operation.
 type Selection interface {
 	Node
 	selection()
@@ -60,6 +70,8 @@ func (*Field) selection()              {}
 func (*FragmentSpread) selection()     {}
 func (*FragmentDefinition) selection() {}
 
+// A Field is a discrete piece of information about an object in the
+// dataset.
 type Field struct {
 	Name         string
 	Alias        string
@@ -68,36 +80,48 @@ type Field struct {
 	SelectionSet SelectionSet
 }
 
+// A FragmentSpread is the instantiation of a FragmentDefinition
+// within some other definition.
 type FragmentSpread struct {
 	Name       string
 	Directives Directives
 }
 
+// A slice of Argument.
 type Arguments []Argument
+
+// An Argument is a key-value pair used to parameterize operations on
+// the dataset.
 type Argument struct {
 	Key   string
 	Value Value
 }
 
+// A slice of Directive.
 type Directives []Directive
+
+// A Directive is a way to describe alternate runtime execution and type
+// validation behavior in a GraphQL document.
 type Directive struct {
 	Name      string
 	Arguments Arguments
 }
 
+// A Value is a container that can hold any data type suitable for an
+// argument.
 type Value interface {
 	Node
 	Value() interface{}
 }
 
-type VariableValue string
-type IntValue int
-type FloatValue float64
-type StringValue string
-type EnumValue string
-type BooleanValue bool
-type ListValue []Value
-type ObjectValue map[string]Value
+type VariableValue string         // A GraphQL variable
+type IntValue int                 // An int
+type FloatValue float64           // A float
+type StringValue string           // A string
+type EnumValue string             // An enum declared somewhere else in the document
+type BooleanValue bool            // A boolean
+type ListValue []Value            // A list of one of the above values
+type ObjectValue map[string]Value // A map of name-value pairs
 
 func (v VariableValue) Value() interface{} { return v }
 func (v IntValue) Value() interface{}      { return v }
@@ -108,6 +132,9 @@ func (v BooleanValue) Value() interface{}  { return v }
 func (v ListValue) Value() interface{}     { return v }
 func (v ObjectValue) Value() interface{}   { return v }
 
-// Node interface
+// An interface implemented by all nodes in the AST to allow serializing
+// them
 type Node interface {
+	// io.WriterTo
+	// fmt.Stringer
 }
